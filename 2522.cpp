f5d275707f494547d193f9af4c6e40ea8a050e3b
wnal1360 <74405315+wnal1360@users.noreply.github.com>
2020-11-17 23:03:44 +0900

    Create 2522.cpp

---
+++ 2522.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int a = n-1;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n - i; j++) {
+			cout << " ";
+		}
+		for (int k = 1; k <= i; k++) {
+			cout << "*";
+		}
+		cout << '\n';
+	}
+	for (int i = 1; i <= n-1; i++) {
+		
+		for (int j = 1; j <=n - a; j++) {
+			cout << " ";
+		}
+		for (int k = 1; k <= a; k++) {
+			cout << "*";
+		}
+		a--;
+		cout << '\n';
+	}
+}
