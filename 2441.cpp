ab6aa962f3789d683e4a6257c33313ac8f46021e
wnal1360 <74405315+wnal1360@users.noreply.github.com>
2020-11-17 23:03:11 +0900

    Create 2441.cpp

---
+++ 2441.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n,a;
+	cin >> n;
+	a=n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n-a; j++) {
+			cout << " ";
+		}
+		for (int k = 1; k <= a; k++) {
+			cout << "*";
+		}
+		a--;
+		cout << '\n';
+	}
+}
