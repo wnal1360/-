f34b9bbd6a284ab45bc6c8a28e8eeacc1f1baf4e
wnal1360 <74405315+wnal1360@users.noreply.github.com>
2020-11-17 23:03:05 +0900

    Create 2440.cpp

---
+++ 2440.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n,a;
+	cin >> n;
+	a=n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= a; j++) {
+			cout << "*";
+		}
+		a--;
+		cout << '\n';
+	}
+}
