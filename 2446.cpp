49a51346df5dc4c8f7c0d6b61836d6fdfcd98f2c
wnal1360 <74405315+wnal1360@users.noreply.github.com>
2020-11-18 19:09:43 +0900

    Create 2446.cpp

---
+++ 2446.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n, a;
+	cin >> n;
+	int b = 1;
+	a = n ;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n - a; j++) {
+			cout << " ";
+		}
+		for (int k = 1; k <= 2*a-1; k++) {
+			cout << "*";
+		}
+		a--;
+		cout << '\n';
+	}
+	for (int i = n - 1; i >= 1; i--) {
+		for (int j = 1; j <= n - b-1; j++) {
+			cout << " ";
+		}
+		for (int k = 1; k <= 2 * b+1; k++) {
+			cout << "*";
+		}
+		b++;	
+		if(i!=1)cout << '\n';
+	}
+}
