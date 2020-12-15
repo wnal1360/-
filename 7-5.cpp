707cd034e8e1c592a531602077f16d2daa5ad85f
wnal1360 <74405315+wnal1360@users.noreply.github.com>
2020-12-16 00:00:03 +0900

    Create 7-5.cpp

---
+++ 7-5.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+int arr[10] = { -1 };
+int cp[10];
+int main() {
+	int cnt = 0;
+	cout << "Enter the numbers : ";
+	for (int i = 0; i < 10; i++) {
+		int n;
+		cin >> n;
+		if (n == arr[n]) continue;
+		arr[n] = n;
+		cp[cnt] = n;
+		cnt++;
+	}
+	cout << "The distinct number are : ";
+	for (int i = 0; i <cnt; i++) {
+		cout << cp[i] << " ";
+	}
+	
+	return 0;
+}
