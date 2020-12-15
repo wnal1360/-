5bc535961915ad36b4fa0bebddb92de0a1cfc4c7
wnal1360 <74405315+wnal1360@users.noreply.github.com>
2020-12-15 23:59:54 +0900

    Create 7-15.cpp

---
+++ 7-15.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+const int a = 100;
+bool lck[a];
+int main() {
+	for (int i = 0; i < a; i++) {
+		int sdt = 0;
+		for (int j = 1; j <= a; j++) {
+			if (j % (i + 1) == 0) {
+				if (lck[j - 1]) lck[j - 1] = false;
+				else lck[j - 1] = true;
+			}
+		}
+	}
+	for (int i = 0; i < a; i++) {
+		if (lck[i]) cout << i + 1 << " ";
+	}
+
+}
