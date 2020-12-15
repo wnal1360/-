9f7744a88060a5cabbc50d228ec9aed70c3800e1
wnal1360 <74405315+wnal1360@users.noreply.github.com>
2020-12-15 23:59:50 +0900

    Create 7-11.cpp

---
+++ 7-11.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+double mean(const double x[], int size);
+double deviation(const double x[], int size);
+const int s = 10;
+double arr[s];
+int main() {	
+	cout << "숫자를 입력하세요. : ";
+	for (int i = 0; i < s; i++) {
+		cin >> arr[i];
+	}
+	cout << " 평균 : " << mean(arr, s) << '\n';
+	cout << "표준편차 : " << deviation(arr, s) << '\n';
+}
+double mean(const double x[], int size) {
+	double sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += x[i];
+	}
+	return sum / size;
+}
+double deviation(const double x[], int size) {
+	double a = 0;
+	double sum = 0;
+	for (int i = 0; i < size; i++) {
+		 a = sqrt(sum += pow(x[i] - mean(arr,s),2) / (size -1));
+	}
+	return a;
+}
