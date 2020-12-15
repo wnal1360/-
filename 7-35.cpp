331ac0f42e5d20cca36e0f96bfcea45008604b6f
wnal1360 <74405315+wnal1360@users.noreply.github.com>
2020-12-16 00:00:01 +0900

    Create 7-35.cpp

---
+++ 7-35.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int countLetters(const char s[]);
+int main() {
+	cout << "문자를 입력하세요 : ";
+	char cstr[100];
+	cin.getline(cstr, 100, '\n');
+	cout << cstr << " 의 글자수는 " << countLetters(cstr) << '\n';
+}
+int countLetters(const char s[]) {
+	int size = strlen(s);
+	int cnt = 0;
+	for (int i = 0; i < size; i++) {
+		if (s[i] >= 'A' and s[i] <= 'Z') cnt++;
+		else if (s[i] >= 'a' and s[i] < 'z') cnt++;
+		else continue;
+	}
+	return cnt;
+}
